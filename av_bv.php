026152c42d32bd2562e905a9b79d66dd4e066ce8
hay2018g <hay2018g@gmail.com>
2022-02-09 21:54:01 +0800

    版本日志

---
+++ av_bv.php
@@ -0,0 +1,4 @@
+<a href="https://player.bilibili.com/player.html?aid=<?php echo $_POST["av"]; ?>">av</a></p>
+<a href="https://player.bilibili.com/player.html?bvid=<?php echo $_POST["bv"]; ?>">bv</a></p>
+<a href="<?php echo $_POST["jump2"] ?>">jump</a></p>
+
